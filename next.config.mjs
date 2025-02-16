52a7fcac439117acd3da811076d6b7601a3c4141
cmanfel <35326337+cmanfel@users.noreply.github.com>
2025-02-16 10:42:48 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  srcDir: 'src', // Tell Next.js to look inside the 'src' directory
+};
 
 export default nextConfig;
